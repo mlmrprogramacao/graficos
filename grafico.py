c4e66d32a5cfa26944abd2a353e0d466d7f5b247
mlmrprogramacao <marialuizamratto@gmail.com>
2025-05-29 12:13:53 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -1,7 +1,41 @@
 import pandas as pd
-import matplotlib.pyplot as plt 
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
 
-try:
-  tot_registros = df.count()['total']
-except
-  df = pd.read_csv9'
+st.title("Circular bar plot do arco-íris!")
+
+df = pd.read_csv('https://perfil-i.ibict.br/media/uploads/user_sum.csv')
+
+if 'total' in df.columns and 'month' in df.columns:
+    df = df.sort_values(by='total')
+
+    categories = df['month']
+    values = df['total']
+    N = len(df)
+
+    angles = np.linspace(0, 2 * np.pi, N, endpoint=False)
+
+    fig, ax = plt.subplots(figsize=(10,10), subplot_kw={'polar': True})
+
+    ax.set_facecolor('black')
+    fig.patch.set_facecolor('black')
+
+    colors = plt.cm.rainbow(np.linspace(0, 1, N))
+
+    bars = ax.bar(angles, values, width=2*np.pi/N, bottom=10, color=colors, edgecolor='white')
+
+    ax.set_xticks(angles)
+    ax.set_xticklabels(categories, fontsize=12, color='white')
+
+    ax.set_yticklabels([])
+
+    ax.set_title("Circular bar plot do arco-íris!", y=1.08,
+                 fontsize=18, color='white')
+
+    ax.tick_params(axis='x', colors='white')
+
+    plt.tight_layout()
+    st.pyplot(fig)
+else:
+    st.warning("As colunas 'month' e 'total' não estão disponíveis.")
